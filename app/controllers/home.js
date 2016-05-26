bb190008da68bf8fefd7fe793f643bf5fd7522c7
Neil Mclaughlin <neil.mclaughlin@agileyorkshire.org>
2016-05-27 00:10:17 +0100

    :hospital: Add display of GP details

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,15 +1,52 @@
 var express = require('express'),
   router = express.Router(),
-  Article = require('../models/article');
+  http = require('http'),
+  util = require('util');
+
+
+var getGpDetails = function getGpDetails (url, callback) {
+
+    http.get(url, function(res){
+
+    var body = '';
+  
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+  
+    res.on('end', function(){
+      callback( JSON.parse(body) );
+    });
+  }).on('error', function(e){
+  	console.log("Got an error: ", e);
+  });
+
+}
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
-  var articles = [new Article(), new Article()];
-    res.render('index', {
-      title: 'Connecting To Services',
-      articles: articles
-    });
+router.get('/gpdetails/:gpId', function (req, res, next) {
+    var url = getSyndicationUrl(req);
+    getGpDetails(url, render(res));
 });
+
+var render = ( function(r) {
+
+  return function(response) {
+    r.render('index', {
+      title: 'Connecting 2 Services',
+      gpDetails:  response
+    });
+  };
+});
+
+var getSyndicationUrl = function (req) {
+    var url = util.format(
+      'http://v1.syndication.nhschoices.nhs.uk/organisations/gppractices/%s.json?apikey=%s',
+      req.params.gpId,
+      req.query.apikey);
+    console.log(url);
+    return url;
+}
