0f06c65bfaae9e9f46a919b95bf6a750f8bcaf48
steve <st3v3n.hunt@gmail.com>
2016-06-06 11:24:35 +0100

    :mag: Add ESLint and make files pass

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,49 +3,45 @@ var express = require('express'),
   http = require('http'),
   util = require('util');
 
-
-var getGpDetails = function getGpDetails (url, callback) {
-
-    http.get(url, function(res){
-
-    var body = '';
-  
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-  
-    res.on('end', function(){
-      callback( JSON.parse(body) );
-    });
-  }).on('error', function(e){
-  	console.log("Got an error: ", e);
-  });
-
-}
-
-module.exports = function (app) {
+module.exports = function(app) {
   app.use('/', router);
 };
 
-router.get('/gpdetails/:gpId', function (req, res, next) {
-    var url = getSyndicationUrl(req);
-    getGpDetails(url, render(res));
+router.get('/gpdetails/:gpId', function(req, res) {
+  var url = getSyndicationUrl(req);
+  getGpDetails(url, render(res));
 });
 
-var render = ( function(r) {
+var getGpDetails = function getGpDetails(url, callback) {
+  http.get(url, function(res) {
 
+    var body = '';
+
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      callback(JSON.parse(body));
+    });
+  }).on('error', function(e) {
+    console.log('Got an error: ', e);
+  });
+};
+
+var render = function(r) {
   return function(response) {
     r.render('index', {
       title: 'GP Details',
-      gpDetails:  response
+      gpDetails: response
     });
   };
-});
+};
 
-var getSyndicationUrl = function (req) {
-    var url = util.format(
-      'http://v1.syndication.nhschoices.nhs.uk/organisations/gppractices/%s.json?apikey=%s',
-      req.params.gpId,
-      req.query.apikey);
-    return url;
-}
+var getSyndicationUrl = function(req) {
+  var url = util.format(
+    'http://v1.syndication.nhschoices.nhs.uk/organisations/gppractices/%s.json?apikey=%s',
+    req.params.gpId,
+    req.query.apikey);
+  return url;
+};
