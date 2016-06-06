4b3fe1575562f801bf198afe97d5d9b628f5a5fb
Neil Mclaughlin <neil.mclaughlin@agileyorkshire.org>
2016-06-06 16:55:09 +0100

    :bar_chart: Add Google analytics
    Added google analytics to master layout to start collecting stats
    on usage. The tool may change so used a js file rather than placing
    the js directly in the template.

---
+++ public/js/analytics.js
@@ -0,0 +1,13 @@
+/*eslint-disable */
+
+//script from:
+//https://analytics.google.com/analytics/web/#management/Settings/a67365892w117922625p123341464/%3Fm.page%3DTrackingCode/
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-67365892-6', 'auto');
+ga('send', 'pageview');
+i/*eslint-enable */
