c0e3b855c693e12048c9df23235bea6b8a7bf19c
steven hunt <st3v3n.hunt@gmail.com>
2016-07-27 17:38:54 +0100

    :bath: Remove GP stuff

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const app = require('./server.js');
-const populateCacheSync = require('./app/lib/populateCache.js');
-
-populateCacheSync('data/gp-system-supplier.csv');
 
 app.listen(app.port, () => {
   console.log(`Express server listening on port ${app.port}`);
