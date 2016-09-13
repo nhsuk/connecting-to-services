a6a51e09d9199647857c2d37968daaeecc1d5904
steven hunt <st3v3n.hunt@gmail.com>
2016-09-13 10:34:37 +0100

    :postbox: Respond with error message for invalid postcodes

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const Postcode = require('postcode');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/results-open', (req, res) => {
+  const location = req.query.location;
+  const postcode = new Postcode(location);
+
+  let message = '';
+
+  if (postcode.valid()) {
+    message = location;
+  } else {
+    message = `${location} is not a valid postcode, please try again`;
+  }
+
+  res.send(message);
 });
 
 module.exports = app;
--- test/server.js
+++ test/server.js
@@ -8,15 +8,31 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Application routes', () => {
+describe('The application', () => {
   it('should mirror the postcode', (done) => {
+    const validPostcode = 'AB12 3CD';
     chai.request(app)
-      .get('/')
+      .get('/results-open')
+      .query({ location: validPostcode })
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         expect(res).to.be.html;
-        expect(res.text).to.equal('Hello World!');
+        expect(res.text).to.equal(validPostcode);
+        done();
+      });
+  });
+
+  it('should validate the postcode and show error message', (done) => {
+    const invalidPostcode = 'invalid';
+    chai.request(app)
+      .get('/results-open')
+      .query({ location: invalidPostcode })
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+        expect(res.text).to.contain(`${invalidPostcode} is not a valid postcode, please try again`);
         done();
       });
   });
