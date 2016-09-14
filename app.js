3cc9b9946dbf6fbe954917f19b3a65b0f69a1728
steven hunt <st3v3n.hunt@gmail.com>
2016-09-14 11:46:42 +0100

    :pager: Make initial request to syndication

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const locationValidator = require('./lib/locationValidator');
+const validateLocation = require('./lib/locationValidator');
 const render = require('./lib/renderer');
+const http = require('http');
 
 const app = express();
 
+function getResults(req, res, next) {
+  const postcode = req.query.location;
+  const apikey = process.env.NHSCHOICES_SYNDICATION_APIKEY;
+
+  http
+    .get(`http://v1.syndication.nhschoices.nhs.uk/organisations/pharmacies/postcode/${postcode}?apikey=${apikey}`, () => {
+      next();
+    });
+}
+
 app.get('/results-open',
-  locationValidator,
+  validateLocation,
+  // go get results
+  getResults,
   render
 );
 
--- test/server.js
+++ test/server.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
+const nock = require('nock');
 
 const expect = chai.expect;
 
@@ -15,16 +16,58 @@ function checkHtmlResponse(err, res) {
 }
 
 describe('The results-open route', () => {
+  const route = '/results-open';
+
+  let originalUrl = '';
+  let originalApikey = '';
+  const baseUrl = 'http://v1.syndication.nhschoices.nhs.uk';
+  const apikey = 'secret';
+
+  before('setup environment variables', () => {
+    originalUrl = process.env.NHSCHOICES_SYNDICATION_URL;
+    originalApikey = process.env.NHSCHOICES_SYNDICATION_APIKEY;
+
+    process.env.NHSCHOICES_SYNDICATION_URL = baseUrl;
+    process.env.NHSCHOICES_SYNDICATION_APIKEY = apikey;
+  });
+
+  after('reset environemnt variables', () => {
+    process.env.NHSCHOICES_SYNDICATION_URL = originalUrl;
+    process.env.NHSCHOICES_SYNDICATION_APIKEY = originalApikey;
+  });
+
   describe('happy paths', () => {
+    const validPostcode = 'AB123CD';
+    const requestPath =
+      new RegExp(`/organisations/pharmacies/postcode/${validPostcode}\\?apikey=${apikey}`);
+
     it('should respond with the postcode, when it is valid', (done) => {
-      const validPostcode = 'AB12 3CD';
+      nock(baseUrl)
+        .get(requestPath)
+        .reply(200, {});
 
       chai.request(app)
-        .get('/results-open')
+        .get(route)
         .query({ location: validPostcode })
         .end((err, res) => {
           checkHtmlResponse(err, res);
           expect(res.text).to.equal(validPostcode);
+          done();
+        });
+    });
+
+    it('should make a request to the syndication API with the supplied postcode', (done) => {
+      const expectedAPICall =
+        nock(baseUrl)
+        .get(requestPath)
+        .reply(200, {});
+
+      chai.request(app)
+        .get(route)
+        .query({ location: validPostcode })
+        .end((err, res) => {
+          checkHtmlResponse(err, res);
+          expect(expectedAPICall.isDone()).to.be.true;
           done();
         });
     });
@@ -37,7 +80,7 @@ describe('The results-open route', () => {
         `${invalidPostcode} is not a valid postcode, please try again`;
 
       chai.request(app)
-        .get('/results-open')
+        .get(route)
         .query({ location: invalidPostcode })
         .end((err, res) => {
           checkHtmlResponse(err, res);
@@ -48,7 +91,7 @@ describe('The results-open route', () => {
 
     it('should check a location is supplied and return an error message', (done) => {
       chai.request(app)
-        .get('/results-open')
+        .get(route)
         .end((err, res) => {
           checkHtmlResponse(err, res);
           expect(res.text).to.contain('A valid postcode is required to progress');
