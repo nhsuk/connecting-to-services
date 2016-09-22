ad84526201fbd062f5b3492604e801b981cd165e
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2016-09-22 16:36:15 +0100

    :memo: Handle missing opening times

--- app/lib/openingTimesParser.js
+++ app/lib/openingTimesParser.js
@@ -23,33 +23,34 @@ const parseOpeningTimesFromSyndicationXml = (openingTimesType, xml) => {
 
     let openingTimesForType = null;
     try {
-      const allOpeningTimes =
-        result.feed.entry[0].content[0].overview[0].openingTimes[0].timesSessionTypes[0];
+      if (result.feed.entry[0].content[0].overview[0].openingTimes) {
+        const allOpeningTimes =
+          result.feed.entry[0].content[0].overview[0].openingTimes[0].timesSessionTypes[0];
 
+        openingTimesForType = jsonQuery('timesSessionType[*:isType]', {
+          data: allOpeningTimes,
+          locals: {
+            isType: (item) => item.$.sessionType === openingTimesType,
+          },
+        }).value[0];
 
-      openingTimesForType = jsonQuery('timesSessionType[*:isType]', {
-        data: allOpeningTimes,
-        locals: {
-          isType: (item) => item.$.sessionType === openingTimesType,
-        },
-      }).value[0];
-
-      openingTimesForType.daysOfWeek[0].dayOfWeek.forEach((item) => {
-        const dayName = item.dayName[0].toLowerCase();
-        openingTimes[dayName] = {};
-        openingTimes[dayName] = {
-          times: [],
-        };
-        item.timesSessions[0].timesSession.forEach((t) => {
-          if (t.fromTime) {
-            // session details are a time range
-            openingTimes[dayName].times.push({ fromTime: t.fromTime[0], toTime: t.toTime[0] });
-          } else {
-            // session details are text (e.g. closed)
-            openingTimes[dayName].times.push(t);
-          }
+        openingTimesForType.daysOfWeek[0].dayOfWeek.forEach((item) => {
+          const dayName = item.dayName[0].toLowerCase();
+          openingTimes[dayName] = {};
+          openingTimes[dayName] = {
+            times: [],
+          };
+          item.timesSessions[0].timesSession.forEach((t) => {
+            if (t.fromTime) {
+              // session details are a time range
+              openingTimes[dayName].times.push({ fromTime: t.fromTime[0], toTime: t.toTime[0] });
+            } else {
+              // session details are text (e.g. closed)
+              openingTimes[dayName].times.push(t);
+            }
+          });
         });
-      });
+      }
     } catch (e) {
       throw new Verror(e, `Unable to get '${openingTimesType}' opening times from xml`);
     }
