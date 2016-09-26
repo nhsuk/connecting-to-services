3ad6f8cda82cb3f222f4d9dd48875099565d9f73
steven hunt <st3v3n.hunt@gmail.com>
2016-09-26 16:04:17 +0100

    :cool: Remove dotenv in favour of env vars

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const config = require('./config/config');
 const configExpress = require('./config/express');
-const dotenv = require('dotenv');
 const requireEnv = require('require-environment-variables');
 
 const app = express();
 
 module.exports = (() => {
-  dotenv.config({ path: './env/.env' });
   requireEnv(['NHSCHOICES_SYNDICATION_APIKEY']);
   requireEnv(['NHSCHOICES_SYNDICATION_BASEURL']);
   configExpress(app, config);
