b6385b960b86ce570fc2842c671dd45277746f2e
steven hunt <st3v3n.hunt@gmail.com>
2016-11-01 16:13:28 +0000

    :interrobang: Add exceptional opening times support

--- app/lib/getPharmacies.js
+++ app/lib/getPharmacies.js
@@ -61,7 +61,11 @@ function nearby(searchPoint, geo, limit) {
     let openingTimesMessage;
 
     if (openingTimes) {
-      const openingTimesMoment = new OpeningTimes(item.openingTimes.general, 'Europe/London');
+      const openingTimesMoment =
+        new OpeningTimes(
+          item.openingTimes.general,
+          'Europe/London',
+          item.openingTimes.alterations);
 
       openingTimesMessage = openingTimesMoment.getOpeningHoursMessage(now);
       isOpen = openingTimesMoment.isOpen(now);
