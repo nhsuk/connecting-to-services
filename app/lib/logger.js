ebb405a61f39ceb0eacdbc0f5fe74bf5f2715b36
steven hunt <st3v3n.hunt@gmail.com>
2016-11-04 09:35:23 +0000

    :calling: Allow log level to be set by env vars

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -1,4 +1,4 @@
-const utils = require('./loggerUtils');
+const loggerUtils = require('./loggerUtils');
 const bunyan = require('bunyan');
 
 const env = process.env.NODE_ENV || 'development';
@@ -6,8 +6,8 @@ const env = process.env.NODE_ENV || 'development';
 const log = bunyan.createLogger({
   name: 'finders',
   serializers: bunyan.stdSerializers,
-  level: utils.getLogLevel(env),
-  streams: utils.getStreams(env),
+  level: loggerUtils.getLogLevel(env),
+  streams: loggerUtils.getStreams(env),
 });
 
 log.info({ logger: log }, `Created logger for ${env}.`);
