424f09ae123e2265359996eab0d3d98a1b6099c7
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2016-11-08 18:11:55 +0000

    :bullettrain_front: Use optimised OpeningTimes

--- app/lib/getPharmacies.js
+++ app/lib/getPharmacies.js
@@ -7,6 +7,43 @@ const utils = require('../lib/utils');
 
 const metersInAMile = 1609;
 
+function getOpeningHoursMessage(status) {
+  if (status.isOpen) {
+    const closedNext = status.until;
+    const closedTime = closedNext.format('h:mm a');
+    const closedDay = closedNext.calendar(status.moment, {
+      sameDay: '[today]',
+      nextDay: '[tomorrow]',
+      nextWeek: 'dddd',
+      lastDay: '[yesterday]',
+      lastWeek: '[last] dddd',
+      sameElse: 'DD/MM/YYYY',
+    });
+    return (
+      ((closedDay === 'tomorrow' && closedTime === '12:00 am')
+        || (closedDay === 'today' && closedTime === '11:59 pm')) ?
+        'Open until midnight' :
+        `Open until ${closedTime} ${closedDay}`);
+  }
+  const openNext = status.until;
+  if (!openNext) {
+    return 'Call for opening times.';
+  }
+  const timeUntilOpen = openNext.diff(status.moment, 'minutes');
+  const openDay = openNext.calendar(status.moment, {
+    sameDay: '[today]',
+    nextDay: '[tomorrow]',
+    nextWeek: 'dddd',
+    lastDay: '[yesterday]',
+    lastWeek: '[last] dddd',
+    sameElse: 'DD/MM/YYYY',
+  });
+  return (
+    (timeUntilOpen <= 60) ?
+      `Opening in ${timeUntilOpen} minutes` :
+      `Closed until ${openNext.format('h:mm a')} ${openDay}`);
+}
+
 function sortByDistance(a, b) {
   return a.distanceInMiles - b.distanceInMiles;
 }
@@ -67,8 +104,9 @@ function nearby(searchPoint, geo, limit) {
           'Europe/London',
           item.openingTimes.alterations);
 
-      openingTimesMessage = openingTimesMoment.getOpeningHoursMessage(now);
-      isOpen = openingTimesMoment.isOpen(now);
+      const status = openingTimesMoment.getStatus(now);
+      openingTimesMessage = getOpeningHoursMessage(status);
+      isOpen = status.isOpen;
     } else {
       openingTimesMessage = 'Call for opening times';
       isOpen = false;
