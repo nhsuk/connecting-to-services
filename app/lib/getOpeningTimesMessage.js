9c985a43bbeba082a6ed5ae2101c2a4aa9aa27c8
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2016-11-10 11:22:21 +0000

    :full_moon: Refactor for simplicity

--- app/lib/getOpeningTimesMessage.js
+++ app/lib/getOpeningTimesMessage.js
@@ -1,24 +1,23 @@
-function closesAtMidnightToday(closedDay, closedTime) {
-  return (
-    (closedDay === 'tomorrow' && closedTime === '12:00 am')
-    || (closedDay === 'today' && closedTime === '11:59 pm')
-  );
+function closesAtMidnight(moment) {
+  const time = moment.format('HH:mm');
+  return (time === '00:00' || time === '23:59');
 }
 
 function getOpeningHoursMessage(status) {
+  const dayDescriptors = {
+    sameDay: '[today]',
+    nextDay: '[tomorrow]',
+    nextWeek: 'dddd',
+    lastDay: '[yesterday]',
+    lastWeek: '[last] dddd',
+    sameElse: 'DD/MM/YYYY',
+  };
   if (status.isOpen) {
     const closedNext = status.until;
     const closedTime = closedNext.format('h:mm a');
-    const closedDay = closedNext.calendar(status.moment, {
-      sameDay: '[today]',
-      nextDay: '[tomorrow]',
-      nextWeek: 'dddd',
-      lastDay: '[yesterday]',
-      lastWeek: '[last] dddd',
-      sameElse: 'DD/MM/YYYY',
-    });
+    const closedDay = closedNext.calendar(status.moment, dayDescriptors);
     return (
-      closesAtMidnightToday(closedDay, closedTime) ?
+      closesAtMidnight(closedNext) ?
         'Open until midnight' :
         `Open until ${closedTime} ${closedDay}`);
   }
@@ -27,14 +26,7 @@ function getOpeningHoursMessage(status) {
     return 'Call for opening times.';
   }
   const timeUntilOpen = openNext.diff(status.moment, 'minutes');
-  const openDay = openNext.calendar(status.moment, {
-    sameDay: '[today]',
-    nextDay: '[tomorrow]',
-    nextWeek: 'dddd',
-    lastDay: '[yesterday]',
-    lastWeek: '[last] dddd',
-    sameElse: 'DD/MM/YYYY',
-  });
+  const openDay = openNext.calendar(status.moment, dayDescriptors);
   return (
     (timeUntilOpen <= 60) ?
       `Opening in ${timeUntilOpen} minutes` :
