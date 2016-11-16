2cf12f344b3792c96efb69ff3dbce12834ff0544
steven hunt <st3v3n.hunt@gmail.com>
2016-11-16 18:35:47 +0000

    :outbox_tray: Extract logger to lib for shareability

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -1,15 +1,1 @@
-const loggerUtils = require('./loggerUtils');
-const bunyan = require('bunyan');
-
-const env = process.env.NODE_ENV || 'development';
-
-const log = bunyan.createLogger({
-  name: 'finders',
-  serializers: bunyan.stdSerializers,
-  level: loggerUtils.getLogLevel(env),
-  streams: loggerUtils.getStreams(env),
-});
-
-log.info({ logger: log }, `Created logger for ${env}.`);
-
-module.exports = log;
+module.exports = require('nhsuk-bunyan-logger')('finders');
