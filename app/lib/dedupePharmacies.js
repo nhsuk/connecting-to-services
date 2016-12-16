718afa908c7f9b8184cae7758d4e2e69f299be16
steven hunt <st3v3n.hunt@gmail.com>
2016-12-16 11:18:03 +0000

    :innocent: Return no more than 3 nearby results

--- app/lib/dedupePharmacies.js
+++ app/lib/dedupePharmacies.js
@@ -2,17 +2,14 @@ const constants = require('./constants');
 
 function dedupe(services) {
   const open = services.open[0];
-  const nearby = services.nearby;
-  let filteredNearby;
+  let nearby = services.nearby;
 
-  if (!open) {
-    filteredNearby = nearby.slice(0, constants.numberOfNearbyResultsToDisplay);
-  } else {
-    filteredNearby = nearby.filter(obj => open.identifier !== obj.identifier);
+  if (open) {
+    nearby = nearby.filter(obj => open.identifier !== obj.identifier);
   }
 
   return {
-    nearby: filteredNearby,
+    nearby: nearby.slice(0, constants.numberOfNearbyResultsToDisplay),
     open: services.open,
   };
 }
--- test/unit/lib/dedupePharmacies.js
+++ test/unit/lib/dedupePharmacies.js
@@ -26,7 +26,7 @@ describe('dedupePharmacies', () => {
     });
   });
 
-  describe('when the open item is not one of the 4 nearby items', () => {
+  describe('when there is no open item and there are 4 nearby items', () => {
     it('should remove the fourth nearby item', () => {
       const fourthId = 'AAA04';
       const services = {
@@ -41,6 +41,28 @@ describe('dedupePharmacies', () => {
       const result = dedupe(services);
 
       expect(result.open.length).is.equal(0);
+      expect(result.nearby.length).is.equal(3);
+      result.nearby.forEach((item) => {
+        expect(item.identifier).to.not.equal(fourthId);
+      });
+    });
+  });
+
+  describe('when the open item is not one of the 4 nearby items', () => {
+    it('should remove the fourth nearby item', () => {
+      const fourthId = 'AAA04';
+      const services = {
+        open: [{ identifier: 'AAA05' }],
+        nearby: [
+          { identifier: 'AAA01' },
+          { identifier: 'AAA02' },
+          { identifier: 'AAA03' },
+          { identifier: fourthId },
+        ] };
+
+      const result = dedupe(services);
+
+      expect(result.open.length).is.equal(1);
       expect(result.nearby.length).is.equal(3);
       result.nearby.forEach((item) => {
         expect(item.identifier).to.not.equal(fourthId);
