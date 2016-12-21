54de352a0c72977362cc590dd8a0f1dbb3bcf10b
Michael Cheung <michaelcheung@nhs.net>
2016-12-21 15:33:44 +0000

    :heavy_exclamation_mark: alert the error code

--- public/js/geolocation.js
+++ public/js/geolocation.js
@@ -17,12 +17,12 @@ if (navigator.geolocation) {
         })
         .fail(function (error) {
           $('.location-error').show();
-          console.log(error.code);
+          alert(error.code);
         });
     };
     var geoError = function(error) {
       $('.location-error').show();
-      console.log(error.code);
+      alert(error.code);
     };
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
   });
