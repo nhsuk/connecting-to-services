27523ab4da14ca118bd78faeaa1f8e39e4aa10c2
Adam Graves <adam.graves@nhs.net>
2017-01-23 10:31:49 +0000

    Replace special characters in branch name

--- scripts/docker_ci.sh
+++ scripts/docker_ci.sh
@@ -41,7 +41,7 @@ elif [[ -n "$TRAVIS" ]]; then
 
   echo "Travis detected"
 
-  SANITISED_BRANCH=$( echo "$TRAVIS_BRANCH" | sed 's/\//_/g' )
+  SANITISED_BRANCH=$( echo "$TRAVIS_BRANCH"  | tr -dc '[:alnum:]\/-' | tr '/' '-' | tr '[:upper:]' '[:lower:]' )
   TAGS="$TAGS $SANITISED_BRANCH"
 
   # IF MASTER BRANCH ALWAYS SET THE LATEST TAG
@@ -56,7 +56,7 @@ elif [[ -n "$TRAVIS" ]]; then
   fi
 
 else
-  currentBranch=$(git rev-parse --abbrev-ref HEAD | sed 's/\//_/g')
+  currentBranch=$(git rev-parse --abbrev-ref HEAD | tr -dc '[:alnum:]\/-' | tr '/' '-' | tr '[:upper:]' '[:lower:]' )
   TAGS="${TAGS} $currentBranch"
   if [ "$currentBranch" == "master" ]; then
     TAGS="${TAGS} latest"
