17c0cd0e9bd5b057f4a1f0d20aacafbfbfbd1047
ajthomascouk <alex.thomas@nhs.net>
2017-07-20 10:34:39 +0100

    :nail_care: implement frontend-library

--- config/express.js
+++ config/express.js
@@ -54,12 +54,10 @@ module.exports = (app, config) => {
       styleSrc: [
         '\'self\'',
         '\'unsafe-inline\'',
-        'fast.fonts.net',
-        'https://dhrlmnmyf2njb.cloudfront.net/',
+        'assets.nhs.uk',
       ],
       fontSrc: [
-        'fast.fonts.net',
-        'https://dhrlmnmyf2njb.cloudfront.net/',
+        'assets.nhs.uk',
       ],
       connectSrc: [
         '\'self\'',
