8fa1737d7f13db16ee5a44321512b0704ff7068e
steven hunt <st3v3n.hunt@gmail.com>
2017-11-03 11:51:07 +0000

    :pushpin: Basic client side geolocation

---
+++ public/js/geolocation.js
@@ -0,0 +1,26 @@
+(function (global) {
+  var $ = global.jQuery;
+  var NHSUK = global.NHSUK || {};
+  var $geoLocate = $('.geo-locate');
+  var $geoLocateError = $('.geo-locate--error');
+
+  function success(position) {
+    console.log(position);
+    console.log('Send the coordinates to the app to get some results');
+  }
+
+  function error() {
+    // $geoLocate.hide();
+    $geoLocateError.show();
+  }
+
+  if (navigator.geolocation) {
+    $geoLocate.show();
+
+    $geoLocate.on('click', function() {
+      navigator.geolocation.getCurrentPosition(success, error);
+    });
+  }
+
+  global.NHSUK = NHSUK;
+})(window);
