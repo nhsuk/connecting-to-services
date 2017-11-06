b056f185cc0b3f6afb89c145407b77b0d2fcbd50
Steven Hunt <st3v3n.hunt@gmail.com>
2017-11-06 17:18:40 +0000

    :japanese_goblin: Uglify JS

--- brunch-config.js
+++ brunch-config.js
@@ -18,6 +18,8 @@ module.exports = {
       sourceMaps: false,
       plugins: {
         afterBrunch: [
+          // eslint-disable-next-line no-template-curly-in-string
+          'sleep 1s && for file in public/js/*.js; do ./node_modules/uglify-es/bin/uglifyjs $file -m -c > ${file}.ugly; mv ${file}.ugly $file; done',
           'sleep 1s && yarn map-replace app/views/layout.nunjucks < assets.json && yarn map-replace app/views/includes/foot.nunjucks < assets.json'
         ]
       }
