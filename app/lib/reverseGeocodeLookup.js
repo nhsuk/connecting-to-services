800d2b51d264c1a1c202c14ce2b8598bece3f2c1
Steven Hunt <st3v3n.hunt@gmail.com>
2017-11-21 16:57:26 +0000

    :tanabata_tree: Add metrics for my location search

--- app/lib/reverseGeocodeLookup.js
+++ app/lib/reverseGeocodeLookup.js
@@ -1,16 +1,21 @@
 const handleReverseGeocodeError = require('./handleReverseGeocodeError');
 const locate = require('./locate');
 const routeHelper = require('../middleware/routeHelper');
+const promCounters = require('./promCounters');
 
 async function reverseGeocode(req, res, next) {
+  promCounters.myLocationSearches.inc(1);
   const result =
     await locate.byLatLon(req.query.latitude, req.query.longitude)
       .catch(error => handleReverseGeocodeError(error, next));
   if (!result) {
+    promCounters.outOfAreaMyLocationSearches.inc(1);
     routeHelper.renderNoResultsPage(null, res);
   } else if (result.country === 'England') {
+    promCounters.englishMyLocationSearches.inc(1);
     next();
   } else {
+    promCounters.knownButNotEnglishMyLocationSearches.inc(1);
     // TODO: render no results page - possibly with some country specific messaging
     routeHelper.renderNoResultsPage(null, res);
   }
