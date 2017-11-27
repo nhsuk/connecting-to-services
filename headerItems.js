b32b52caffd2622cf359f599f6b174fff29f3f73
Steven Hunt <st3v3n.hunt@gmail.com>
2017-11-27 15:22:08 +0000

    :hammer: Refactor header items

--- headerItems.js
+++ headerItems.js
@@ -1,35 +1,48 @@
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
+const requireEnv = require('require-environment-variables');
+
+const log = require('./app/lib/logger');
+
+requireEnv(['HEADER_API_URL']);
 
 const headerAPIURL = process.env.HEADER_API_URL;
+const headerFileName = 'app/views/includes/header-items.nunjucks';
+const siteToReplace = 'http://site';
+const nhsukDomain = 'https://www.nhs.uk';
 
 let output = '';
 
 request(headerAPIURL, (error, response, body) => {
   const topLevel = JSON.parse(body);
+
   if (!error && response.statusCode === 200) {
     for (let i = 0; i < topLevel.length; i++) {
       const item = topLevel[i];
-      let itemURL = item.URL.split(',');
-      itemURL = itemURL[0].replace('http://site', 'https://www.nhs.uk');
-      itemURL = itemURL.replace(/ /g, '%20');
-      const subItems = item.Submenus;
+      const itemURL = item.URL.split(',')[0].replace(siteToReplace, nhsukDomain).replace(/ /g, '%20');
+
       output += `<li><a href="${itemURL}">${item.Title}</a>`;
+
+      const subItems = item.Submenus;
+
       if (subItems.length !== 0) {
         output += '<ul>';
-        for (let a = 0; a < subItems.length; a++) {
-          const subItem = subItems[a];
-          let subItemURL = subItem.URL.split(',');
-          subItemURL = subItemURL[0].replace('http://site', 'https://www.nhs.uk');
-          subItemURL = subItemURL.replace(/ /g, '%20');
+        for (let j = 0; j < subItems.length; j++) {
+          const subItem = subItems[j];
+          const subItemURL = subItem.URL.split(',')[0].replace(siteToReplace, nhsukDomain).replace(/ /g, '%20');
+
           output += `<li><a href="${subItemURL}">${subItem.Title}</a>`;
         }
         output += '</ul>';
       }
       output += '</li>';
     }
-    fs.writeFile('app/views/includes/header-items.nunjucks', output, (err) => {
-      if (err) throw err;
+
+    fs.writeFileSync(headerFileName, output, (err) => {
+      if (err) {
+        throw err;
+      }
+      log.info(`${headerFileName} written.`);
     });
   }
 });
