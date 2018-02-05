32b2bce7cf6ed1e4342f232436ca0f25dae430fc
Steven Hunt <st3v3n.hunt@gmail.com>
2018-02-05 08:47:46 +0000

    :worried: Remove unnecessary wrap function

--- app/public/js/isWrapped.js
+++ app/public/js/isWrapped.js
@@ -1,13 +1,6 @@
 (function (global) {
   'use strict';
   var $ = global.jQuery;
-  $('h2.results__name').each(function(){
-    var $height = $(this).height();
-    var $lineHeight = Math.floor(parseInt($(this).css('line-height').replace('px','')));
-    if ($height > $lineHeight){
-      $(this).parents('li').addClass('wrapped');
-    }
-  });
   $('details').on('click', function() {
     $(this).parents('li').toggleClass('open');
   });
