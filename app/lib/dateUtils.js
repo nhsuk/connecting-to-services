c70d9ea37c73bc82f8faae29d3914b7a26a22c67
steven hunt <st3v3n.hunt@gmail.com>
2018-03-21 11:11:56 +0000

    :koala: Refactor for simplicity

--- app/lib/dateUtils.js
+++ app/lib/dateUtils.js
@@ -1,5 +1,12 @@
 const constants = require('./constants');
 
+function cloneMoment(datetimeMoment, hour, minute) {
+  return datetimeMoment.clone()
+    .hour(hour)
+    .minute(minute)
+    .second(0);
+}
+
 function getDateString(dateString) {
   // eslint-disable-next-line no-restricted-globals
   const date = (isNaN(Date.parse(dateString)))
@@ -18,8 +25,29 @@ function isNextOpenTomorrow(nowDateString, nextOpenDateString) {
   return ((nextOpenDate - nowDate) === constants.dayInMilliseconds);
 }
 
+function isTimeOutsideBusinessHours(datetimeMoment) {
+  const businessHoursStart = cloneMoment(
+    datetimeMoment,
+    constants.businessHours.start.hour,
+    constants.businessHours.start.minute
+  );
+  const businessHoursEnd = cloneMoment(
+    datetimeMoment,
+    constants.businessHours.end.hour,
+    constants.businessHours.end.minute
+  );
+  return !datetimeMoment.isBetween(businessHoursStart, businessHoursEnd, null, '[)');
+}
+
+function isWeekday(datetimeMoment) {
+  const dayOfWeek = datetimeMoment.day();
+  return dayOfWeek > 0 && dayOfWeek < 6;
+}
+
 module.exports = {
   getDateString,
   getDay,
   isNextOpenTomorrow,
+  isTimeOutsideBusinessHours,
+  isWeekday,
 };
--- app/lib/displayOpenResults.js
+++ app/lib/displayOpenResults.js
@@ -1,39 +1,32 @@
 const moment = require('moment');
 
+const dateUtils = require('./dateUtils');
 const isRequestForOpenResults = require('./isRequestForOpenResults');
 
-function isInitialRequest(req) {
+function isRequestInitial(req) {
   return !req.query.open;
 }
 
-function isWeekday(datetimeMoment) {
-  const dayOfWeek = datetimeMoment.day();
-  return dayOfWeek > 0 && dayOfWeek < 6;
+function isRequestInitialAndWeekdayOutsideBusinessHours(req, datetimeMoment) {
+  return dateUtils.isWeekday(datetimeMoment)
+    && dateUtils.isTimeOutsideBusinessHours(datetimeMoment)
+    && isRequestInitial(req);
 }
 
-function isTimeOutsideBusinessHours(datetimeMoment) {
-  const businessHoursStart = datetimeMoment.clone().hour(8).minute(0).second(0);
-  const businessHoursEnd = datetimeMoment.clone().hour(18).minute(0).second(0);
-  return !datetimeMoment.isBetween(businessHoursStart, businessHoursEnd, null, '[)');
+function isRequestInitialAndWeekend(req, datetimeMoment) {
+  return !dateUtils.isWeekday(datetimeMoment) && isRequestInitial(req);
+}
+
+function isRequestInitialAndOutsideBusinessHours(req, datetimeMoment) {
+  return isRequestInitialAndWeekdayOutsideBusinessHours(req, datetimeMoment)
+    || isRequestInitialAndWeekend(req, datetimeMoment);
 }
 
 function displayOpenResults(req, datetimeOverride = process.env.DATETIME) {
   const datetimeMoment = datetimeOverride ? moment(datetimeOverride) : moment();
-  let showOpenResults;
-
-  if (isWeekday(datetimeMoment)) {
-    if (isTimeOutsideBusinessHours(datetimeMoment) && isInitialRequest(req)) {
-      showOpenResults = !isRequestForOpenResults(req);
-    } else {
-      showOpenResults = isRequestForOpenResults(req);
-    }
-  } else {
-    showOpenResults = isInitialRequest(req)
-      ? !isRequestForOpenResults(req)
-      : isRequestForOpenResults(req);
-  }
-
-  return showOpenResults;
+  return isRequestInitialAndOutsideBusinessHours(req, datetimeMoment)
+    ? !isRequestForOpenResults(req)
+    : isRequestForOpenResults(req);
 }
 
 module.exports = displayOpenResults;
