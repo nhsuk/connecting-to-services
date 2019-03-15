15c2864cb070ed32bfb5c02cfb7b6e4a9bb5761a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-03-15 13:39:13 +0000

    :boom: WIP - Hardcode cookiebot banner script

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
   },
   cookiebot: {
-    scriptUrl: process.env.COOKIEBOT_SCRIPT_URL || '//assets.nhs.uk/scripts/cookie-consent.js',
+    scriptUrl: '//assets-staging.nhs.uk/staging/scripts/cookie-consent.js',
   },
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
