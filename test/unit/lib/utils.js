c57a4303fbc906ae26038cc9b47c8e607055ed90
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-08-22 11:20:38 +0100

    :eyes: Improve test descriptions

--- test/unit/lib/utils.js
+++ test/unit/lib/utils.js
@@ -5,26 +5,32 @@ const expect = chai.expect;
 
 describe('utils', () => {
   describe('deepClone', () => {
-    it('should clone value properties', () => {
+    describe('value properties', () => {
       const objToClone = { a: 'a' };
-
       const clone = utils.deepClone(objToClone);
 
-      expect(clone).to.deep.equal(objToClone);
+      it('should clone', () => {
+        expect(clone).to.deep.equal(objToClone);
+      });
 
-      clone.a = 'new value';
-      expect(clone).to.not.deep.equal(objToClone);
+      it('should not affect original object', () => {
+        clone.a = 'new value';
+        expect(clone).to.not.deep.equal(objToClone);
+      });
     });
 
-    it('should clone object properties', () => {
+    describe('object properties', () => {
       const objToClone = { a: { b: 'b' } };
-
       const clone = utils.deepClone(objToClone);
 
-      expect(clone).to.deep.equal(objToClone);
+      it('should clone object properties', () => {
+        expect(clone).to.deep.equal(objToClone);
+      });
 
-      clone.a = 'new value';
-      expect(clone).to.not.deep.equal(objToClone);
+      it('should not affect original object', () => {
+        clone.a = 'new value';
+        expect(clone).to.not.deep.equal(objToClone);
+      });
     });
   });
 });
