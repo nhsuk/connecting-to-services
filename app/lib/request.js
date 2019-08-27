fceb17385eb82b442a7ef89546bbc93aa59f800d
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-08-27 08:32:19 +0100

    :black_nib: More succinct request

--- app/lib/request.js
+++ app/lib/request.js
@@ -1,16 +1,8 @@
 const rp = require('request-promise-native');
 
-// const headers = require('./headers');
-// const search = require('../../../config/config').search;
-
-async function request(url, headers = {}, query = {}, method = 'Get') {
-  const response = await rp({
-    body: JSON.stringify(query),
-    headers,
-    method,
-    url,
-  });
-  return JSON.parse(response);
-}
-
-module.exports = request;
+module.exports = async (url, headers = {}, query = {}, method = 'Get') => JSON.parse(await rp({
+  body: JSON.stringify(query),
+  headers,
+  method,
+  url,
+}));
