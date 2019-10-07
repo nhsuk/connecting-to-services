b7f54acb6bd29a92cb5b63ff37be14f28be78227
Tom Doughty <thomashdoughty@gmail.com>
2019-10-07 20:34:08 +0100

    :boom: Remove unecessary JS

--- app/public/js/init.js
+++ app/public/js/init.js
@@ -1,10 +1,3 @@
 // Require all modules needed for the application
 require('./analytics');
 require('./geoLocation');
-require('./headerMobileMenuToggle');
-require('./headerMobileSearchToggle');
-require('./headerSearchTypeahead');
-require('./isWrapped');
-require('./menuToggle');
-require('./openToggle');
-require('./skiplinkFix');
