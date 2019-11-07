e20791c5aee8d5c279e56ea44b4bb8bf248b8411
Nigel Atwell <nigel.atwell@outlook.com>
2019-11-07 11:12:03 +0000

    Updated root url for service-search

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -3,33 +3,50 @@ module.exports = {
     nearbyResultsCount: 10,
     openResultsCount: 10,
     paths: {
-      nearby: 'nearby',
-      open: 'open',
-    },
+      nearby: "nearby",
+      open: "open"
+    }
   },
   app: {
-    description: 'Find an open pharmacy near you on the NHS website. Check your local pharmacy\'s opening times, and find your nearest late night, 24-hour or out-of-hours chemist.',
-    locale: 'en_GB',
-    siteName: 'nhs.uk',
-    title: 'Find a pharmacy',
+    description:
+      "Find an open pharmacy near you on the NHS website. Check your local pharmacy's opening times, and find your nearest late night, 24-hour or out-of-hours chemist.",
+    locale: "en_GB",
+    siteName: "nhs.uk",
+    title: "Find a pharmacy",
     twitter: {
-      card: 'summary_large_image',
-      creator: '@nhsuk',
-      site: '@nhsuk',
-    },
+      card: "summary_large_image",
+      creator: "@nhsuk",
+      site: "@nhsuk"
+    }
   },
-  assetsUrl: 'https://assets.nhs.uk',
-  daysOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  daysOfWeekOrderedForUi: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-  daysOfWorkingWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-  placeSearch: 'place',
-  postcodeSearch: 'postcode',
+  assetsUrl: "https://assets.nhs.uk",
+  daysOfWeek: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ],
+  daysOfWeekOrderedForUi: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ],
+  daysOfWorkingWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+  placeSearch: "place",
+  postcodeSearch: "postcode",
   promHistogramBuckets: [0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 1, 1.5, 5, 10],
   queryTypes: {
-    nearby: 'nearby',
-    openNearby: 'openNearby',
+    nearby: "nearby",
+    openNearby: "openNearby"
   },
-  siteRoot: '/find-a-pharmacy',
-  yourLocation: 'your location',
-  yourLocationSearch: 'location',
+  siteRoot: "service-search/find-a-pharmacy",
+  yourLocation: "your location",
+  yourLocationSearch: "location"
 };
